4c1d726acf8a7950bf7419554b73bbb38d49f420
cristiano <britonano@gmail.com>
2021-03-26 20:53:55 -0300

    Add files via upload

---
+++ LOOPS-01/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  //contando de -5 ate  6
+  int count;
+  for(count=-5; count<6; count=count+1)
+  {
+     printf("%d\n",count);
+  }
+  system("PAUSE");	
+  return 0;
+}
