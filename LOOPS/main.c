2e1f6eb5af10ef5154743d5550b2202bb078bbb4
cristiano <britonano@gmail.com>
2021-03-26 20:49:38 -0300

    Add files via upload

---
+++ LOOPS/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  int x;
+  for(x=0; x<10; x=x+1)
+  {
+      puts("CONTAGEM ");
+  }
+  system("PAUSE");	
+  return 0;
+}
