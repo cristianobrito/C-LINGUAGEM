09ceefd13b112b687372fea51ea323fde772d3ce
cristiano <britonano@gmail.com>
2021-03-29 19:27:03 -0300

    Add files via upload

---
+++ LOOP-WHILE/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  
+  int x;
+  x = 0;
+  while(x < 10)
+  {
+    puts("vou executando loop while!!");
+    x = x + 1;
+  }
+  system("PAUSE");	
+  return 0;
+}
