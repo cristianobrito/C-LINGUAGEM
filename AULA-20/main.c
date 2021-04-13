658d2efeaac164ba2a0e9582d4a68221e3efe1aa
cristiano <britonano@gmail.com>
2021-04-13 13:02:58 -0300

    Add files via upload

---
+++ AULA-20/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("%-9s me\n","meio");
+    printf("%-8s me\n","meio");
+    printf("%-7s me\n","meio");
+    printf("%-6s me\n","meio");
+    printf("%-5s me\n","meio");
+    printf("%-4s me\n","meio");
+    return 0;
+}
