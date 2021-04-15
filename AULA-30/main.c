24698d5e641a6eebd576f84f736f491b2e3141fb
cristiano <britonano@gmail.com>
2021-04-15 14:36:51 -0300

    Add files via upload

---
+++ AULA-30/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE 5
+
+struct bot
+{
+    int xpos;
+    int ypos;
+};
+struct bot initialize(struct bot b);
+int main()
+{
+    struct bot robots[SIZE];
+    int x;
+    srandom((unsigned)time(NULL));
+    for(x=0;x<SIZE;x++)
+    {
+        robots[x] = initialize(robots[x]);
+        printf("robot %d: coordenadas: %d,%d\n",
+               x+1,robots[x].xpos,robots[x].ypos);
+    }
+    struct bot initialize(struct bot b)
+    {
+        int x,y;
+        x = random();
+        y = random();
+        x%=20;
+        y%=20;
+        b.xpos = x;
+        b.ypos = y;
+        return(b);
+    };
+
+}
